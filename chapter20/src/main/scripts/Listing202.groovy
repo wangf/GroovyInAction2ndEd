1b6037d3f5db63ed6c77297c5f2fcb2e8042632e
gordonad <gordon@gordondickens.com>
2013-09-17 08:40:37 -0400

    ActiveX seems to work after some Grab configuration

--- chapter20/src/main/scripts/Listing202.groovy
+++ chapter20/src/main/scripts/Listing202.groovy
@@ -1,3 +1,8 @@
+@Grab(group = 'org.codehaus.groovy.modules.scriptom', module = 'scriptom', version = '1.6.0')
+@GrabExclude("org.codehaus.groovy:groovy")
+
+import org.codehaus.groovy.scriptom.ActiveXObject
+
 def explorer = new ActiveXObject('InternetExplorer.Application')
 explorer.Visible = true
 explorer.AddressBar = true
