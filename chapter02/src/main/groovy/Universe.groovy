832fd0d77e09811cd7ed076c5e56daee14ac1c72
gordonad <gordon@gordondickens.com>
2015-02-07 11:58:33 -0500

    Chapter 2: 07-Feb-2015

---
+++ chapter02/src/main/groovy/Universe.groovy
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2015. Confidential, unpublished property of CIGNA.
+ * Do not duplicate or distribute. Use and distribution limited solely to authorized personnel.
+ */
+
+/**
+ * Project: groovy-in-action-2nd
+ *
+ * Created by Gordon Dickens (m86014)
+ * Create Date: 2/7/15
+ */
+class Universe {
+//    @groovy.transform.TypeChecked
+    int answer() { "forty two" }
+}
